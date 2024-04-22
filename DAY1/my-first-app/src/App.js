ff952c83e56bc6ffbc8b5a65f02cb0c5cf6f623d
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-04-22 10:37:17 +0530

    ✨ using CRA

---
+++ DAY1/my-first-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <div>
+          <header>{/* <h1> Hello CRA !</h1> */}</header>
+        </div>
+        <div>Another Child !</div>
+      </>
+    );
+  }
+}
+export default App;
