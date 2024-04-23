02983f60beb0190d1fd55f9eb56d0ca15aa92833
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-04-23 09:33:55 +0530

    ✨ Using Rating (star)

--- DAy2/onlineshoppingapp/src/components/product/product.component.jsx
+++ DAy2/onlineshoppingapp/src/components/product/product.component.jsx
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 
 export default class Product extends Component {
   render() {
+    const rating = [];
+    for (let index = 0; index < this.props.productdetails.rating; index++) {
+      <i className="fa-solid fa-star"></i>;
+      rating.push(<i className="fa-solid fa-star"></i>);
+    }
     return (
       <div className="col-md-3">
         <div className="card">
@@ -15,12 +20,7 @@ export default class Product extends Component {
             <h5 className="card-title">{this.props.productdetails.name}</h5>
             <p className="card-text">₹. {this.props.productdetails.price}</p>
             <p className="card-text">
-              <span style={{ color: "orange" }}>
-                <i className="fa-solid fa-star"></i>
-                <i className="fa-solid fa-star"></i>
-                <i className="fa-solid fa-star"></i>
-                <i className="fa-solid fa-star"></i>
-              </span>
+              <span style={{ color: "orange" }}>{rating}</span>
             </p>
             <button className="btn btn-primary">
               {" "}
