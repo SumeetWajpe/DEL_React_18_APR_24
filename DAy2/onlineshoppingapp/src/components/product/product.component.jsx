d9cc789875931fa838c2c08c1df2a430e269debd
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-04-24 08:34:47 +0530

    ✨ Passing DeleteAProduct to product component

--- DAy2/onlineshoppingapp/src/components/product/product.component.jsx
+++ DAy2/onlineshoppingapp/src/components/product/product.component.jsx
@@ -39,6 +39,14 @@ export default class Product extends Component {
               {" "}
               {this.state.currLikes} <i className="fa-solid fa-thumbs-up"></i>
             </button>
+            <button
+              className="btn btn-danger mx-1"
+              onClick={() =>
+                this.props.DeleteAProduct(this.props.productdetails.id)
+              }
+            >
+              <i class="fa-solid fa-trash"></i>
+            </button>
           </div>
         </div>
       </div>
