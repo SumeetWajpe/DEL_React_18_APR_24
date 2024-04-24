0187304635b67e367ac542e5fd6cc22bd8ed99ab
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-04-24 12:24:23 +0530

    ✨ using useEffect in posts & understanding dependency list

--- DAy2/onlineshoppingapp/src/components/posts/posts.functional.jsx
+++ DAy2/onlineshoppingapp/src/components/posts/posts.functional.jsx
@@ -1,16 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
   useEffect(() => {
     axios
       .get("https://jsonplaceholder.typicode.com/posts")
-      .then(res => console.log(res.data));
-  });
+      .then(res => setPosts(res.data));
+  }, []);
   return (
     <div>
       <header>
         <h1>All Posts</h1>
       </header>
+      <main>
+        <ul className="list-group">
+          {posts.map(p => (
+            <li key={p.id}>{p.title}</li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
 };
