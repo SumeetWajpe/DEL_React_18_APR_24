ff93b78071f92d2d8fd38d462df944d3465ff821
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-04-29 12:47:30 +0530

    ✨ defining reducers

--- DAY5/onlineshoppingapp_redux/src/redux/reducers/products.reducer.js
+++ DAY5/onlineshoppingapp_redux/src/redux/reducers/products.reducer.js
@@ -1,15 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  products: [],
-};
-createSlice({
+const initialState = [];
+const productSlice = createSlice({
   name: "products",
   initialState,
   reducers: {
     incrementLikes: (store, action) => {
       console.log("Within increment likes reducer !");
+      // biz logic to update store
       return store;
     },
   },
 });
+
+export const { incrementLikes } = productSlice.actions;
+export default productSlice.reducer;
