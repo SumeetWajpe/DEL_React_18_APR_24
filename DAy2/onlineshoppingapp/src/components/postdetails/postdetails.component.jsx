b0b576db0556dc9d56ee10a59f76bc31229bd33e
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-04-29 09:00:52 +0530

    ✨ Displaying post details

--- DAy2/onlineshoppingapp/src/components/postdetails/postdetails.component.jsx
+++ DAy2/onlineshoppingapp/src/components/postdetails/postdetails.component.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function PostDetails() {
   const { id } = useParams();
-  return <div>Post Details for {id}</div>;
+  const [thePost, setThePost] = useState({});
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`).then(res => {
+      setThePost(res.data);
+    });
+  });
+  return (
+    <div>
+      <h2>Post Details for {id}</h2>
+      <h4>User Id : {thePost.userId} </h4>
+      <h4>Title : {thePost.title} </h4>
+      <h4> Body : {thePost.body} </h4>
+    </div>
+  );
 }
 
 export default PostDetails;
